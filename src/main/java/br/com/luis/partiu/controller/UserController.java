b6371842358cc3a99065834bf040d0a36ab64a96
Luis Henrique <luishenrique2859@hotmail.com>
2024-01-16 16:57:14 -0300

    feat: adding method update User

--- src/main/java/br/com/luis/partiu/controller/UserController.java
+++ src/main/java/br/com/luis/partiu/controller/UserController.java
@@ -3,6 +3,7 @@ package br.com.luis.partiu.controller;
 
 import br.com.luis.partiu.dto.user.UserRegisterDto;
 import br.com.luis.partiu.dto.user.UserResponseDto;
+import br.com.luis.partiu.dto.user.UserUpdateDto;
 import br.com.luis.partiu.service.UserService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
 
 
 @RestController
@@ -27,5 +29,9 @@ public class UserController {
         return new ResponseEntity<>(service.register(userRegisterDto), HttpStatus.CREATED);
     }
 
-
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity<UserResponseDto> updateUser(@PathVariable("id") UUID id, @RequestBody UserUpdateDto updateDto) {
+        return new ResponseEntity<>(service.updateUser(id, updateDto), HttpStatus.OK);
+    }
 }
--- src/main/java/br/com/luis/partiu/service/UserService.java
+++ src/main/java/br/com/luis/partiu/service/UserService.java
@@ -3,11 +3,13 @@ package br.com.luis.partiu.service;
 
 import br.com.luis.partiu.dto.user.UserRegisterDto;
 import br.com.luis.partiu.dto.user.UserResponseDto;
+import br.com.luis.partiu.dto.user.UserUpdateDto;
 import br.com.luis.partiu.models.User;
 import br.com.luis.partiu.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
 
 
 @Service
@@ -26,6 +28,18 @@ public class UserService {
         return new UserResponseDto(savedUser.getId(), savedUser.getName(), savedUser.getEmail(), savedUser.getPassword(),
                 savedUser.getAvatarUrl(),savedUser.getGender());
     }
-    
+
+    public UserResponseDto updateUser(UUID id, UserUpdateDto updateDto) {
+        User user = repository.findById(id).
+                orElseThrow(() -> new RuntimeException("Id não encontrado"));
+
+        user.setAvatarUrl(updateDto.avatarUrl());
+
+        User updatedUser = repository.save(user);
+
+        return new UserResponseDto(updatedUser.getId(), updatedUser.getName(), updatedUser.getEmail(), updatedUser.getPassword(),
+                updatedUser.getAvatarUrl(),updatedUser.getGender());
+
+    }
 
 }
