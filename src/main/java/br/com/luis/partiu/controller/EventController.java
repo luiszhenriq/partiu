f3d500da8b27eea7103ff8846ab9baa214761667
Luis Henrique <luishenrique2859@hotmail.com>
2024-01-17 17:34:14 -0300

    feat: adding getAllEvents and updateEvent methods

--- src/main/java/br/com/luis/partiu/controller/EventController.java
+++ src/main/java/br/com/luis/partiu/controller/EventController.java
@@ -3,15 +3,17 @@ package br.com.luis.partiu.controller;
 
 import br.com.luis.partiu.dto.event.EventRequestDto;
 import br.com.luis.partiu.dto.event.EventResponseDto;
+import br.com.luis.partiu.dto.event.UpdateEventDto;
 import br.com.luis.partiu.service.EventService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/event")
@@ -26,5 +28,16 @@ public class EventController {
     @Transactional
     public ResponseEntity<EventResponseDto> createEvent(@RequestBody EventRequestDto eventDto) {
         return new ResponseEntity<>(service.createEvent(eventDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<EventResponseDto>> getAllEvents(Pageable pageable) {
+        return new ResponseEntity<>(service.getAllEvents(pageable), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity<EventResponseDto> updateEvent(@PathVariable("id") UUID id, @RequestBody UpdateEventDto updateEventDto) {
+        return new ResponseEntity<>(service.updateEvent(id, updateEventDto), HttpStatus.OK);
     }
 }
--- src/main/java/br/com/luis/partiu/service/EventService.java
+++ src/main/java/br/com/luis/partiu/service/EventService.java
@@ -3,12 +3,19 @@ package br.com.luis.partiu.service;
 
 import br.com.luis.partiu.dto.event.EventRequestDto;
 import br.com.luis.partiu.dto.event.EventResponseDto;
+import br.com.luis.partiu.dto.event.UpdateEventDto;
 import br.com.luis.partiu.models.Event;
 import br.com.luis.partiu.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class EventService {
@@ -17,13 +24,15 @@ public class EventService {
     @Autowired
     private EventRepository repository;
 
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+
     public EventResponseDto createEvent(EventRequestDto eventDto) {
 
         Event newEvent = new Event(eventDto);
 
         Event saveEvent = repository.save(newEvent);
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
         return new  EventResponseDto(saveEvent.getId(),
                 saveEvent.getTitle(),
@@ -32,5 +41,46 @@ public class EventService {
                 saveEvent.getStartAt().format(formatter),
                 saveEvent.getEndsIn().format(formatter),
                 saveEvent.getFee());
+    }
+
+   public Page<EventResponseDto> getAllEvents(Pageable pageable) {
+        Page<Event> eventPage = repository.findAll(pageable);
+
+        List<EventResponseDto> eventList = eventPage.getContent().stream()
+                .map(event -> new EventResponseDto(
+                        event.getId(),
+                        event.getTitle(),
+                        event.getDescription(),
+                        event.getCoverUrl(),
+                        event.getStartAt().format(formatter),
+                        event.getEndsIn().format(formatter),
+                        event.getFee()
+                ))
+                .collect(Collectors.toList());
+
+        return new PageImpl<>(eventList, pageable, eventPage.getTotalElements());
+    }
+
+    public EventResponseDto updateEvent(UUID id, UpdateEventDto updateEventDto) {
+
+        Event event = repository.findById(id).
+                orElseThrow(()-> new RuntimeException("Id não foi encontrado"));
+
+        event.setTitle(updateEventDto.title());
+        event.setDescription(updateEventDto.description());
+        event.setCoverUrl(updateEventDto.coverUrl());
+        event.setStartAt(updateEventDto.startAt());
+        event.setEndsIn(updateEventDto.endsIn());
+        event.setFee(updateEventDto.fee());
+
+        Event updatedEvent = repository.save(event);
+
+        return new  EventResponseDto(updatedEvent.getId(),
+                updatedEvent.getTitle(),
+                updatedEvent.getDescription(),
+                updatedEvent.getCoverUrl(),
+                updatedEvent.getStartAt().format(formatter),
+                updatedEvent.getEndsIn().format(formatter),
+                updatedEvent.getFee());
     }
 }
